a29af0369de856478be04e93a7f64bd176c9ca89
LTRGames <40744764+LTRGames@users.noreply.github.com>
2018-07-02 01:00:54 +0200

    Add files via upload

---
+++ Section02/BullsAndCowsGame/Main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void PrintIntro();
+string GetGuessAndPrintBack();
+
+// the entry point for our application
+int main()
+{
+	PrintIntro();
+
+	// loop for the number of turns asking for guesses
+	constexpr int NUMBER_OF_TURNS = 5;
+	for (int count = 1; count <= NUMBER_OF_TURNS; count++)
+	{
+		GetGuessAndPrintBack();
+		cout << endl;
+	}
+	return 0;
+}
+
+
+// introduce the game
+void PrintIntro() 
+{
+	constexpr int WORD_LENGTH = 5;
+	cout << "Welcome to Bulls and Cows, a fun word game.\n";
+	cout << "Can you guess the " << WORD_LENGTH;
+	cout << " letter isogram I'm thinking off?\n";
+	cout << endl;
+	return;
+}
+
+
+string GetGuessAndPrintBack() 
+{
+// get a guess from the player
+	cout << "Enter your guess: ";
+	string Guess = "";
+	getline (cin,Guess);
+	// print the guess back
+	cout << "Your guess was: " << Guess << endl;
+	return Guess;
+}
