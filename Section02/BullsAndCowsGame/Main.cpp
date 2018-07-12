f2b07cd3138f79fd3d7a62480b6caa0ea9f33f9c
LTRGames <40744764+LTRGames@users.noreply.github.com>
2018-07-12 10:07:58 +0200

    first commit

--- Section02/BullsAndCowsGame/Main.cpp
+++ Section02/BullsAndCowsGame/Main.cpp
@@ -4,43 +4,62 @@
 using namespace std;
 
 void PrintIntro();
-string GetGuessAndPrintBack();
+void PlayGame();
+string GetGuess();
+bool AskToPlayAgain();
 
 // the entry point for our application
 int main()
 {
-	PrintIntro();
-
-	// loop for the number of turns asking for guesses
-	constexpr int NUMBER_OF_TURNS = 5;
-	for (int count = 1; count <= NUMBER_OF_TURNS; count++)
-	{
-		GetGuessAndPrintBack();
-		cout << endl;
+	bool bPlayAgain = false;
+	do {
+		PrintIntro();
+		PlayGame();
+		bPlayAgain = AskToPlayAgain();
 	}
-	return 0;
+	while (bPlayAgain);
+	
+		return 0; // exit the application
 }
 
 
 // introduce the game
-void PrintIntro() 
+void PrintIntro()
 {
-	constexpr int WORD_LENGTH = 5;
+	constexpr int WORLD_LENGTH = 9;
 	cout << "Welcome to Bulls and Cows, a fun word game.\n";
-	cout << "Can you guess the " << WORD_LENGTH;
-	cout << " letter isogram I'm thinking off?\n";
+	cout << "Can you guess the " << WORLD_LENGTH;
+	cout << " letter isogram I'm thinking of?\n";
 	cout << endl;
 	return;
 }
 
 
-string GetGuessAndPrintBack() 
+void PlayGame()
 {
-// get a guess from the player
+	// loop for the number of turns asking for guesses
+	constexpr int NUMBER_OF_TURNS = 5;
+	for (int count = 1; count <= NUMBER_OF_TURNS; count++) {
+		string Guess = GetGuess();
+		cout << "Your guess was: " << Guess << endl;
+		cout << endl;
+	}
+}
+
+
+string GetGuess()
+{
+	// get a guess from the player
 	cout << "Enter your guess: ";
 	string Guess = "";
-	getline (cin,Guess);
-	// print the guess back
-	cout << "Your guess was: " << Guess << endl;
+	getline(cin, Guess);
 	return Guess;
+}
+
+bool AskToPlayAgain()
+{
+	cout << "Do you want to play again (y/n)? ";
+	string Response = "";
+	getline(cin, Response);
+	return (Response[0] == 'y') || (Response[0] == 'Y');
 }
